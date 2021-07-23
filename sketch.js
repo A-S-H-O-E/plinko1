3dcb6c48328c47c34c5b7b57f418b32a9bbf7bdc
A-S-H-O-E <84203572+A-S-H-O-E@users.noreply.github.com>
2021-07-23 13:06:06 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,10 @@
+function setup() {
+  createCanvas(800, 800);
+  var arr = [10, 13, 20, 23, 30, 37, 40, 50, 60];
+  for (var i = 0; i < arr.length; i = i + 1) {
+    console.log(arr[i])
+  }
+}
+function draw() {
+  background("black");
+}
